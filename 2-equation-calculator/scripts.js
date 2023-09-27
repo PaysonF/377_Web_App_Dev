f9d147cbdf42efc315cafa4a74b0ade8392e6646
PaysonF <pfowler24@hanoverstudents.org>
2023-09-26 23:24:51 -0400

    Directory Work

--- 2-equation-calculator/scripts.js
+++ 2-equation-calculator/scripts.js
@@ -1,6 +1,36 @@
-
 function DetermineCalc(equation) {
-    
+    $('#first').show()
+    $('#second').show()
+    $('#third').show()
+    if (equation == "Area") {
+        $('#area').show()
+        $('#math').hide()
+        $('#perimeter').hide()
+        $('#volume').hide()
+
+        $('#third').hide()
+    } else if (equation == "Perimeter") {
+        $('#perimeter').show()
+        $('#math').hide()
+        $('#area').hide()
+        $('#volume').hide()
+
+        $('#third').hide()
+    } else if (equation == "Volume") {
+        $('#volume').show()
+        $('#math').hide()
+        $('#area').hide()
+        $('#perimeter').hide()
+    } else {
+        $('#math').hide()
+        $('#area').hide()
+        $('#perimeter').hide()
+        $('#volume').hide()
+
+        $('#first').hide()
+        $('#second').hide()
+        $('#third').hide()
+    }
 }
 
 function MathCalc() {
@@ -8,13 +38,13 @@ function MathCalc() {
 }
 
 function AreaCalc() {
-
+var answer = first * second
 }
 
 function PerimeterCalc() {
-
+var answer = first * 2 + second * 2
 }
 
 function VolumeCalc() {
-
+var answer = first * second * third
 }
