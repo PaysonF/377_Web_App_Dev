f3bf461bf40da6e31a58e8da7c5ec87f48cf6598
Payson Fowler <pfowler24@hanoverstudents.org>
2023-09-29 11:23:58 -0400

    changes

--- AOC-2022/day1.py
+++ AOC-2022/day1.py
@@ -1,12 +1,55 @@
-file = open('day1-exe.txt', 'r')
+file = open('day1.txt', 'r')
 lines = file.readlines()
-var total = 0
+
+total = 0
+max1 = 0
 for line in lines:
     line = line.strip()
-
     if line =='':
         print('New Elf')
-        print
+        if total > max1:
+            max1 == total
+        print(total)
+        total = 0
     else:
-        print(line)
+        total += int(line)
+        if total > max1:
+            max1 = total
+print('max1 is ' + str(max1))
+
+total2 = 0
+max2 = 0
+for line in lines:
+    line = line.strip()
+    if line =='':
+        print('New Elf')
+        if total2 > max2 and total2 < max1:
+            max2 == total2
+        print(total2)
+        total2 = 0
+    else:
+        total2 += int(line)
+        if total2 > max2 and total2 < max1:
+            max2 = total2
+print('max2 is ' + str(max2))
+
+total3 = 0
+max3 = 0
+for line in lines:
+    line = line.strip()
+    if line =='':
+        print('New Elf')
+        if total3 > max3 and total3 < max2:
+            max3 == total3
+        print(total3)
+        total3 = 0
+    else:
+        total3 += int(line)
+        if total3 > max3 and total3 < max2:
+            max3 = total3
+print('max3 is ' + str(max3))
+true_total = max1+max2+max3
+print('The Top Three Combined Is ' + str(true_total))
+
+
     
