d4c541935e0daf954afe36e3dbfa1e1419b12544
Payson Fowler <pfowler24@hanoverstudents.org>
2023-10-13 11:24:56 -0400

    changesmore

--- AOC-2022/Day3/day3.py
+++ AOC-2022/Day3/day3.py
@@ -1,22 +1,34 @@
 file = open('day3-exe.txt', 'r')
-bag = file.readlines()
-numb = 0
-letter = ""
-total = 0
+lines = file.readlines()
 LetterList = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
-for line in bag:
-    strip_string = bag[numb].strip()
-    first_part = strip_string[:len(strip_string)//2]
-    second_part = strip_string[len(strip_string)//2:]
-    #print(first_part)
-    #print(second_part)
-    numb += 1
-    for char in range(len(first_part)):
-        for other in range(len(second_part)):
-            if first_part[char] == second_part[other]:
-                letter = first_part[char]
-                for tweet in range(len(LetterList)):
-                    if letter == LetterList[tweet]:
-                        total += tweet
-                        print(total)
+total = 0
+def first_part():
+    total = 0
+    for line in lines:
+        line = line.strip()
+        length = len(line)
+        compartment_1 = line[:length // 2]
+        compartment_2 = line[length // 2:]
+        for letter in compartment_1:
+            if letter in compartment_2:
+                break
+        if ord(letter) < 96:
+            total += ord(letter) - 64 + 26
+        else:
+            total += ord(letter) - 96
+    print(total)
+def second_part():
+    i = 0
+    for line in lines:
+        if i % 3 == 0 and i > 0:
+           # bag_1 = line[i - 3]
+           # bag_2 = line[i - 2]
+           # bag_3 = line[i - 1]
+           print(line)
 
+        i += 1
+
+        
+        
+#first_part()
+second_part()
---
+++ AOC-2022/Day4/day4.py
@@ -0,0 +1,6 @@
+file = open('day4-exe.txt')
+lines = file.readLines()
+
+for line in lines
+    line = line.strip()
+    print(line)
