c264753e041d9b36a2b768e8246cd65c817c443c
PaysonF <pfowler24@hanoverstudents.org>
2023-10-13 02:20:44 -0400

    day3pychanges

--- AOC-2022/Day3/day3.py
+++ AOC-2022/Day3/day3.py
@@ -1,12 +1,22 @@
 file = open('day3-exe.txt', 'r')
 bag = file.readlines()
 numb = 0
+letter = ""
+total = 0
+LetterList = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
 for line in bag:
-    compart = bag[numb].split()
-    #look up half thing code
-    print(compart[0])
-    #c1 = (compart[0])
-    #c2 = (compart[1])
+    strip_string = bag[numb].strip()
+    first_part = strip_string[:len(strip_string)//2]
+    second_part = strip_string[len(strip_string)//2:]
+    #print(first_part)
+    #print(second_part)
     numb += 1
-
+    for char in range(len(first_part)):
+        for other in range(len(second_part)):
+            if first_part[char] == second_part[other]:
+                letter = first_part[char]
+                for tweet in range(len(LetterList)):
+                    if letter == LetterList[tweet]:
+                        total += tweet
+                        print(total)
 
