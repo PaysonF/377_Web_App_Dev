23f997da4f18464d17dcf74b87db68b0c8d98296
PaysonF <pfowler24@hanoverstudents.org>
2023-10-18 15:16:09 -0400

    sty

--- AOC-2022/Day3/day3.py
+++ AOC-2022/Day3/day3.py
@@ -1,7 +1,7 @@
-file = open('day3-exe.txt', 'r')
+file = open('day3.txt', 'r')
 lines = file.readlines()
 LetterList = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
-total = 0
+threelist = []
 def first_part():
     total = 0
     for line in lines:
@@ -19,16 +19,38 @@ def first_part():
     print(total)
 def second_part():
     i = 0
+    total = 0
     for line in lines:
-        if i % 3 == 0 and i > 0:
-           # bag_1 = line[i - 3]
-           # bag_2 = line[i - 2]
-           # bag_3 = line[i - 1]
-           print(line)
-
-        i += 1
-
-        
-        
+        pos = 0
+        threelist.insert(i, line)
+        if len(threelist) == 3:
+            A = threelist[0]
+            B = threelist[1]
+            C = threelist[2]
+            for x in range(3):
+                if len(threelist[x]) > pos:
+                    pos = x
+                    print(pos)
+                    
+            if pos == 0:
+                for letter in A:
+                    if letter in B and C:
+                        break
+            elif pos == 1:
+                for letter in B:
+                    if letter in A and C:
+                        break
+            else:
+                for letter in C:
+                    if letter in A and B:
+                        break
+            if ord(letter) < 96:
+                total += ord(letter) - 38
+            else:
+                total += ord(letter) - 96
+            threelist.clear()
+        i+=1
+    print(total)
+          
 #first_part()
 second_part()
