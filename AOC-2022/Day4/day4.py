0239332d05e76f96080a6e77d539228fde72797a
Payson Fowler <pfowler24@hanoverstudents.org>
2023-10-20 11:23:07 -0400

    day4fin and day5st

--- AOC-2022/Day4/day4.py
+++ AOC-2022/Day4/day4.py
@@ -1,19 +1,29 @@
-file = open('day4-exe.txt')
+file = open('day4.txt')
 lines = file.readlines()
-first = []
-second = []
 
+total = 0
+total2 = 0
 for line in lines:
-    thing = 0
     line = line.strip()
-    for letter in line:
-        if letter == ",":
-            for x in range(thing):
-                first.insert(0, )
-                print(first)
-        
-        thing += 1
-        
+
+    elf1, elf2 = line.split(',')
+
+    x1, x2 = [int(num)for num in elf1.split('-')]
+    y1, y2 = [int(num)for num in elf2.split('-')]
+
+    if x1 <= y1 and y2 <= x2:
+        total += 1
+    elif y1 <= x1 and x2 <= y2:
+        total += 1
+    if  (x1 >= y1 and x1 <= y2) or \
+        (x2 >= y1 and x2 <= y2) or \
+        (x1 < y1 and x2 > y2):
+        total2 += 1
+    
+print(total2)
+
+    
+
 
             
             
---
+++ AOC-2022/Day5/day5.py
@@ -0,0 +1,5 @@
+file = open('day4.txt')
+lines = file.readlines()
+
+for line in lines:
+    line = line.strip()
