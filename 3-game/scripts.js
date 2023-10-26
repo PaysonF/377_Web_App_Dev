375e920a057792a7f8add4d07e2393d3a942ae44
Payson Fowler <pfowler24@hanoverstudents.org>
2023-10-26 11:23:34 -0400

    cannonsemiworking

--- 3-game/scripts.js
+++ 3-game/scripts.js
@@ -1,10 +1,23 @@
+document.onkeydown = changeAim;
+var angleAmount = 0;
+var start = false;
 function hideTitle(){
     $('#mainmenu').hide()
-    cannon = document.getElementById("barrel");
-    cannon.setAttribute("transform", "rotate(0.5)");
+    start = true;
 }
     
-function changeAim(){
-    alert("wpw")
+function changeAim(e){
+    //console.log(start);
+    if (start == true){
+        if (e.key == "ArrowRight"){
+            console.log("right");
+            angleAmount += -5
+        }else if(e.key == "ArrowLeft"){
+            console.log("Left");
+            angleAmount += 5
+        }
+        cannon = document.getElementById("barrel");
+        cannon.setAttribute("transform", "rotate(" + angleAmount + ",500, 10)");
+    }
 }
     
