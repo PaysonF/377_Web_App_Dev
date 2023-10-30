b413db9d86b9504df9be065e6db569a3bf84e349
PaysonF <pfowler24@hanoverstudents.org>
2023-10-30 07:25:58 -0400

    weekendupdatestuff

--- 3-game/scripts.js
+++ 3-game/scripts.js
@@ -1,23 +1,33 @@
 document.onkeydown = changeAim;
 var angleAmount = 0;
 var start = false;
+var midfire = false;
 function hideTitle(){
     $('#mainmenu').hide()
     start = true;
 }
-    
+
+function shoot(){
+    alert("PEW")
+    //midfire = true;
+}
+
+
+
 function changeAim(e){
-    //console.log(start);
-    if (start == true){
-        if (e.key == "ArrowRight"){
+    console.log(angleAmount);
+    if (start == true && midfire == false){
+        if (e.key == "ArrowRight" && angleAmount < 81){
             console.log("right");
-            angleAmount += -5
-        }else if(e.key == "ArrowLeft"){
+            angleAmount += 2
+        }else if(e.key == "ArrowLeft" && angleAmount > 0){
             console.log("Left");
-            angleAmount += 5
+            angleAmount += -2
+        } else if(e.key == " "){
+            shoot();
         }
         cannon = document.getElementById("barrel");
-        cannon.setAttribute("transform", "rotate(" + angleAmount + ",500, 10)");
+        cannon.setAttribute("transform", "rotate(" + angleAmount + ",118.4, 686)");
     }
 }
     
