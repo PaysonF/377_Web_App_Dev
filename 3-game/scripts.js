b6afcde53dd8810872efa4fa8b0c0d9f55f3f33d
Payson Fowler <pfowler24@hanoverstudents.org>
2023-11-02 11:23:34 -0400

    cannonfixes

--- 3-game/scripts.js
+++ 3-game/scripts.js
@@ -6,6 +6,9 @@ var angleAmount = 0;
 var start = false;
 var midfire = false;
 var tutorial = false;
+var angley = 20;
+var anglex = 10;
+var expansion = 15;
 // at start of code initialze hiding tutorial stufff (group) checkmark
 
 //edit gameStart, make mainmenu show during tutorial and still work
@@ -29,21 +32,40 @@ function tutStart(){
     cannon = document.getElementById("barrel");
 }
 
+function boomAnimation(){
+    $("#ball").attr('r', expansion);
+    if(expansion < 29){
+        expansion += 1;
+    } else{
+        clearInterval(boomIntervalId)
+    }
+}
+
+var shootIntervalId = 0;
+var thing = 25;
 function shoot(){
-    alert("PEW")
-    ballAnimation();
+    $("#ball").attr('cx', 118.4);
+    $("#ball").attr('cy', 686);
+    shootIntervalId = setInterval(ballAnimation, 20);
 }
 
 function ballAnimation(){
-    // $( "#ball" ).animate({
-    //     x: "+=100"
-    //   }, 5000, function() {
-    //     // Animation complete.
-    //   });
-
-    $("#ball").animate(
-        {svgTransform: 'translate(300 -300)'}, 2000); 
-  }
+    
+    var x = parseInt($("#ball").attr('cx'));
+    var y = parseInt($("#ball").attr('cy'));
+    if (x > 700) {
+        clearInterval(shootIntervalId)
+        boomIntervalId = setInterval(boomAnimation, 20);
+    }
+    console.log(angley);
+    $("#ball").attr('cx', x + anglex);
+    $("#ball").attr('cy', y - angley);
+    angley -= 2;
+    if (anglex > 0){
+        anglex -= 0.5;
+    }
+    
+}
 
 
 
@@ -57,6 +79,8 @@ function changeAim(e){
             console.log("Left");
             angleAmount += -2
         } else if(e.key == " "){
+            angley += angleAmount;
+            anglex += angleAmount;
             shoot();
         }
         cannon = document.getElementById("barrel");
