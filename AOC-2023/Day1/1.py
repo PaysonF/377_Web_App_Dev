d15f8fa6b637b83628464da7ff690d2380299340
Payson Fowler <pfowler24@hanoverstudents.org>
2023-12-01 11:23:20 -0500

    messofcode1

--- AOC-2023/Day1/1.py
+++ AOC-2023/Day1/1.py
@@ -1,13 +1,15 @@
 file = open('1-exe.txt', 'r')
 lines = file.readlines()
-
+#use if isdigit()
 numlist = ['0','1','2','3','4','5','6','7','8','9']
+tsilmun = numlist[::-1]
+print(tsilmun)
 rev = len(numlist)
 offset = 0
 checknumb = 0
 
 total = 0
-
+truetotal = 0
 
 dig1 = 0
 dig2 = 0
@@ -22,21 +24,19 @@ for line in lines:
             if checknumb == numlist[x] and dig1 == 0:
                 dig1 = checknumb
                 print('dig1 in')
-        # checks for second number
-        for x in range(len(numlist)):
-            if rev != 2:
-                rev -= 1
-                if checknumb == numlist[rev] and dig2 == 0:
-                    dig2 = checknumb
-                    print('dig2 in') 
-            
+            if checknumb == tsilmun[x] and dig2 == 0:
+                dig2 = checknumb
+                print('dig2 in')
         offset += 1
         if offset == len(line):
             offset = 0
-            dig1 = 0
-            dig2 = 0
     # adds dig1 and dig2 then prints         
     print('NEW LINE')
-    total += int(str(dig1) + str(dig2))
-print(total)
+    total = str(dig1) + str(dig2)
+    print(total + 'wow')
+    truetotal += int(total)
+    dig1 = 0
+    dig2 = 0
+print(truetotal)
+
         
