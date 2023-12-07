97f3403ad6f24b7d4c06a7203ccd17e2cda649bc
Payson Fowler <pfowler24@hanoverstudents.org>
2023-12-07 11:24:40 -0500

    almost there

--- AOC-2023/Day2/2.py
+++ AOC-2023/Day2/2.py
@@ -1,18 +1,34 @@
-file = open('2-exe.txt', 'r')
+file = open('2.txt', 'r')
 lines = file.readlines()
 
-game_possible = True
-
-def checkwin(ab):
-    print(ab)
-
+addIds = 0
+Possible = True
+thing = 0
+PowerTotal = 0
 
 for line in lines:
-    line = line.strip()
-    line = line.split(';')
-    for x in range(len(line)):
-        checkwin(line)
-    
-print('Red Cubes:')
-print('Green Cubes:')
-print('Blue Cubes:')
+    Possible = True
+    maxblue = 0
+    maxred = 0
+    maxgreen = 0
+    for round in line.split(':')[1].split(';'):
+        for color_count in [ x.strip() for x in round.split(',') ]:
+            count, color = [ entry.strip() for entry in color_count.split(' ')]
+            if color == 'blue' and int(count) > 14 or color == 'red' and int(count) > 12 or color == 'green' and int(count) > 13:
+                Possible = False
+            if color == 'blue' and int(count) > maxblue:
+                maxblue = int(count)
+            if color == 'red' and int(count) > maxred:
+                maxred = int(count)
+            if color == 'green' and int(count) > maxgreen:
+                maxgreen = int(count)
+            SubPowerTotal = maxblue * maxred
+            SubPowerTotal *= maxgreen
+        PowerTotal += SubPowerTotal
+    if Possible == True:
+        addIds += thing       
+    thing += 1
+print('Addids Total: ' + str(addIds))
+print('Power total: ' + str(PowerTotal))        
+                
+                
