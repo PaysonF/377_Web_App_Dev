4a05f72e5ffa67189a0afda27f114de85d3317f5
Payson Fowler <pfowler24@hanoverstudents.org>
2024-01-03 08:43:50 -0500

    Final

--- Sql/Quiz Sql.sql
+++ Sql/Quiz Sql.sql
@@ -46,31 +46,35 @@ AND stu_status = 'Active'
 AND att_date LIKE '2022-09%'
 ;
 
-
-
 -- Q6: How many section of History 6 does Sebastian
 --     Matthews teach? (2 points)
--- A6:
-SELECT *
+-- A6: Four Different Sections
+SELECT COUNT(DISTINCT cls_section)
 FROM teachers
-WHERE tch_first_name = 'Sebastian'
-AND tch_last_name = 'Matthews'
+JOIN classes ON cls_tch_id = tch_id
+WHERE tch_first_name = 'Sebastian' AND tch_last_name = 'Matthews' AND cls_course = 'History 6'
 ;
-SELECT * FROM schedules;
 -- Q7: How many sections of History 6 are being taught
 --     overall and what is the total enrollment across
 --     all those sections? (3 points)
--- A7:
-SELECT *
-FROM schedules
-WHERE
+-- A7: Total 252 students spread across twelve different sections
+SELECT COUNT(DISTINCT cls_id) AS 'Num Sections', COUNT(*) AS 'Total Students'
+FROM classes
+JOIN schedules ON ssc_cls_id = cls_id
+JOIN students ON stu_id = ssc_stu_id
+WHERE cls_course = 'History 6'
 ;
 -- Q8: What is the first student, alphabetically by last
 --     name, on the roster for History 6, section 11?
 --     (5 points)
--- A8:
+-- A8: Emilia Amis
 SELECT *
 FROM students
+JOIN schedules on ssc_stu_id = stu_id
+JOIN classes on cls_id = ssc_cls_id
+WHERE cls_course = 'History 6' AND cls_section = '11'
+ORDER BY stu_last_name
+LIMIT 1
 ;
 -- Q9: There was a new hire in the guidance department.
 --     Write an UPDATE statement that will change students
@@ -81,12 +85,16 @@ SET stu_counselor = 'Rodriguez, Oscar'
 WHERE stu_counselor = 'McDonnell, Kristen'
 ;
 -- A9: Complete, Check Below
-SELECT * FROM students WHERE stu_counselor = 'Rodriguez, Oscar';
-
+SELECT stu_first_name, stu_last_name, stu_counselor FROM students WHERE stu_counselor = 'Rodriguez, Oscar';
 -- Q10: There was bad weather on the morning of Jan 26, 2022.
 --      Write a DELETE statement that will remove all tardy
 --      attendance records (code = 'T') on that date. (2 points)
--- A10:
--- DELETE
-WHERE
+-- A10: Done
+DELETE FROM attendance
+WHERE att_date = '2022-1-26 00:00:00' AND att_code = 'T'
+;
+-- Check
+SELECT *
+FROM attendance
+WHERE att_date = '2022-1-26 00:00:00' AND att_code = 'T'
 ;
