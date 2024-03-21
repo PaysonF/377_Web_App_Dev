32494b775c15118b75b8b4d28da2297637a44077
Payson Fowler <pfowler24@hanoverstudents.org>
2024-03-21 11:22:49 -0400

    PlayerMovement_&_Lists

--- 11-final-project/web/midnight.js
+++ 11-final-project/web/midnight.js
@@ -1,8 +1,11 @@
 document.onkeydown = checkey;
-var px = 300;
-var py = 435;      //document.getElementById("player").getAttribute('cy'); figure out whats wrong with this  
+var px = 0;
+var py = 0;      //document.getElementById("player").getAttribute('cy'); figure out whats wrong with this  
 var movespeed = 5;
-var checklist = ["hello"];
+// All variables below use x,y list for various things, interactList dictates areas where the player should be able to use stuff, movelimit list prevents the player from moving when they are at the edge of a room, lastly transport list is where the "doors are" mabye make into part of the interact list but seperate
+var interactList = ["hello", "Placeholder"];
+var movelimitList = [];
+var transportList = ["Plhold"];
 
 $(document).ready(function() {
     //Code here runs upon page open
@@ -13,39 +16,42 @@ $(document).ready(function() {
 function start(){
     $("#player").fadeIn(3100);
     //test rooms here
-    
 }
 
 //$("#player").attr('cx')
 function checkey(e){
     e.preventDefault();
-    //if (start == true && midfire == false){ make this check if player is not aganist wall, and not in cutscene or anything else
+    //if (player x not in border && player y not in border){ make this check if player is not aganist wall, and not in cutscene or anything else
         //Horizontal Movement
         
+        //Make movement smoother eventually, get rid of stutter at begening
         if (e.key == "ArrowRight" || e.key == "d"){ //Right
             px += movespeed;
         } else if(e.key == "ArrowLeft" || e.key == "a"){ //Left
             px -= movespeed;
-        } else if(e.key == " "){ //Interact
+        } else if(e.key == "ArrowUp" || e.key == "w"){ //Left
+            py -= movespeed;
+        } else if(e.key == "ArrowDown" || e.key == "s"){ //Left
+            py += movespeed;
+        } else if(e.key == ' '){ //Interact
             start();
-            console.log("Space Pressed");
-            
+            console.log("Space Pressed");        
             interact(px, py);
         }
-        console.log("player x: " + $("#player").attr("x"));
+        console.log("player x: " + $("#player").attr("cx"));
         // $("#player").attr("x", px); 
-        //$("#player").attr("transform", "translate(" + px + ",0)");
+        $("#player").attr("transform", "translate(" + px + "," + py + ")");
         console.log(px);
-        $("#player").attr('cx', px);
         // $(".player").attr('cy', py);
         //} 
 }
+function limit_move(){
+    
+}
+
 function interact(x, y){
-    log("Interaction");
-    //Figure out way to check when near
+    console.log(checklist);
     if ("hello" in checklist){
         log("CheckList: Hello")
     }
-}
-
-    
+}   
