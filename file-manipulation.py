0028155329354bb73bd5a2299dd0e0ce13246169
Payson Fowler <pfowler24@hanoverstudents.org>
2024-03-22 11:24:34 -0400

    filedone and midnight title work

--- file-manipulation.py
+++ file-manipulation.py
@@ -1,12 +1,15 @@
 import os
-
+import shutil
 def rename_photos():
     path = "C:/Users/pfowler24/Desktop/Photo"
     prefix = input("What prefix do you want? ")
 
     i = 1
+    print("Old Files - - - - -")
+    print("")
     for filename in os.listdir(path):
         extension = filename.split(".")[1].lower()
+
         if extension in ["jfif", "png", "gif"]:
             print(filename)
 
@@ -15,4 +18,26 @@ def rename_photos():
 
             os.rename(source, destination)
             i += 1
-rename_photos()
+    print("")
+    print("Updated Files - - - - - ")
+    print("")
+    for filename in os.listdir(path):
+        if extension in ["jfif", "png", "gif"]:
+            print(filename)
+def copy_file():
+    original = "C:/Users/pfowler24/Desktop/Photo/photo1.jfif"
+    filename, extension = original.split('.')
+    for i in range(5):
+        copy_filename = filename + ' - Copy' + str(i + 1) + "." + extension
+        print(copy_filename)
+        shutil.copyfile(original, copy_filename)
+choice = ""
+while choice != "rename" or choice != "clone" or choice != "copy":
+    choice = input("What function do you want? ")
+    if choice == "rename":
+        rename_photos()
+    if choice == "clone" or choice == "copy":
+        copy_file()
+    if choice != "rename" or choice != "clone" or choice != "copy":
+        print("Entered String does not match options, try again")
+        print("- - - - - - - - - -")
