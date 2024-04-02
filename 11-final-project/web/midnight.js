68d9536042b415650730c7c29ac4f814753a3d9b
Payson Fowler <pfowler24@hanoverstudents.org>
2024-04-02 10:21:24 -0400

    PlayerFlipping

--- 11-final-project/web/midnight.js
+++ 11-final-project/web/midnight.js
@@ -3,7 +3,7 @@ var px = 0;
 var py = 0;      //document.getElementById("player").getAttribute('cy'); figure out whats wrong with this  
 var movespeed = 5;
 // All variables below use x,y list for various things, interactList dictates areas where the player should be able to use stuff, movelimit list prevents the player from moving when they are at the edge of a room, lastly transport list is where the "doors are" mabye make into part of the interact list but seperate
-var interactList = ["hello", "Placeholder"];
+var interactList = [50, "Placeholder"];
 var movelimitList = [];
 var transportList = ["Plhold"];
 
@@ -11,8 +11,18 @@ $(document).ready(function() {
     $("#player").fadeIn(100);
 });
 
-function start(){
-    
+function characterChange(current){
+    if (current == "Horizontal"){
+        $("#backArm").attr("transform", "translate(" + 0 + "," + 0 + ")");
+        $("#frontArm").attr("transform", "translate(" + 0 + "," + 0 + ")");
+        $("#backLeg").attr("transform", "translate(" + 0 + "," + 0 + ")");
+        $("#frontLeg").attr("transform", "translate(" + 0 + "," + 0 + ")");
+    } else if (current == "Vertical"){
+        $("#backArm").attr("transform", "translate(" + 10 + "," + 0 + ")");
+        $("#frontArm").attr("transform", "translate(" + -10 + "," + 0 + ")");
+        $("#backLeg").attr("transform", "translate(" + 5 + "," + 0 + ")");
+        $("#frontLeg").attr("transform", "translate(" + -5 + "," + 0 + ")");
+    }
 }
 
 //$("#player").attr('cx')
@@ -22,6 +32,15 @@ function checkey(e){
         //Horizontal Movement
         
         //Make movement smoother eventually, get rid of stutter at begening
+        // checks for what state character should be in
+        if (e.key == "ArrowRight" || e.key == "d" || e.key == "ArrowLeft" || e.key == "a"){
+            //Facing Horizontaly
+            characterChange("Horizontal");
+        } else if (e.key == "ArrowUp" || e.key == "w" || e.key == "ArrowDown" || e.key == "s"){
+            //Facing Verticaly
+            characterChange("Vertical");
+        }
+
         if (e.key == "ArrowRight" || e.key == "d"){ //Right
             px += movespeed;
         } else if(e.key == "ArrowLeft" || e.key == "a"){ //Left
@@ -30,7 +49,9 @@ function checkey(e){
             py -= movespeed;
         } else if(e.key == "ArrowDown" || e.key == "s"){ //Left
             py += movespeed;
-        } else if(e.key == ' '){ //Interact
+        } 
+        
+        else if(e.key == ' '){ //Interact
             console.log("Space Pressed");    
             interact(px, py);
         }
@@ -43,8 +64,8 @@ function checkey(e){
         //} 
 }
 //handle all three
-function limit_move(a, b){
-    var temp = toString(a)
+function limit_move(x, y){
+    var temp = x.toString() + y.toString();
     console.log("temp is: " + temp)
     if (temp in interactList || temp in movelimitList){
         console.log("Dont Move")
