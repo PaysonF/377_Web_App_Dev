95a64a4bfa8fe33d9a95d1a190c8e5e92ce09cfb
PaysonF <pfowler24@hanoverstudents.org>
2024-04-03 22:06:34 -0400

    partialTp&OBJ

--- 11-final-project/web/midnight.js
+++ 11-final-project/web/midnight.js
@@ -1,6 +1,6 @@
 document.onkeydown = checkey;
 var px = 0;
-var py = 0;      //document.getElementById("player").getAttribute('cy'); figure out whats wrong with this  
+var py = 0;    //document.getElementById("player").getAttribute('cy'); figure out whats wrong with this  
 var movespeed = 10;
 // All variables below use x,y list for various things, interactList dictates areas where the player should be able to use stuff, movelimit list prevents the player from moving when they are at the edge of a room, lastly transport list is where the "doors are" mabye make into part of the interact list but seperate
 var interactList = [50, "Placeholder"];
@@ -12,10 +12,28 @@ var maxX = 0;
 var minY = -10;
 var maxY = 50;
 
+// doorways should always be fifty wide
+var room = "starter";
+var hallwaye = ["50,175", "150,175", "150,150", "200,150", "200,175", "550,175", "950,175", "950,215", "975,215", "975,260", "950,260", "950,300", "50,300"];
+var starter = "300,100, 300,200, 200,200, 200,300, 300,300, 600,300, 600,100";
+var elevator = "300,150, 400,150, 400,100, 475,100, 475,150, 525,150, 525,100, 600,100, 600,150, 700,150, 700,400, 300,400";
+var cafe = "50,400 950,400 950,100 50,100";
+var tube = "400,25 600,25 600,475 400,475";
+
+
+var hallway = hallwaye.toString()
+    .replace("[", "")  //remove the right bracket
+    .replace("]", "")
 
 $(document).ready(function() {
     $("#player").fadeIn(100);
+    document.getElementById("Rooms").setAttribute("points", starter);
 });
+
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
 
 function characterChange(current){
     if (current == "Horizontal"){
@@ -39,7 +57,8 @@ function checkey(e){
         
         //Make movement smoother eventually, get rid of stutter at begening
         // checks for what state character should be in
-
+   
+    console.log($("#f3Hallway").attr("points"));
     if (e.key == "ArrowRight" || e.key == "d" || e.key == "ArrowLeft" || e.key == "a"){
             //Facing Horizontaly
             characterChange("Horizontal");
@@ -59,8 +78,13 @@ function checkey(e){
     } 
         
     else if(e.key == ' '){ //Interact
-        console.log("Space Pressed");    
+        console.log("room: " + room);
         interact(px, py);
+        if (room == "hallway"){
+            roomChange(starter); 
+        } else {
+            roomChange(hallway);
+        }
     }
     limit_move(px, py);
     console.log("player y: " + py + ", MinY is:" + minY);
@@ -69,6 +93,7 @@ function checkey(e){
     // make interaction / transportation points work though - check before preventing movement - and variable check
 
     if (py != minY || py != maxY){
+        //document.getElementById("Rooms").setAttribute("transform", "translate(" + px + "," + py + ")");
         $("#player").attr("transform", "translate(" + px + "," + py + ")");
     }
     // $(".player").attr('cy', py);
@@ -76,16 +101,27 @@ function checkey(e){
 }
 //handle all three
 function limit_move(x, y){
-    var temp = x.toString() + y.toString();
-    console.log("temp is: " + temp)
-    if (temp in interactList || temp in movelimitList){
-        console.log("Dont Move")
-    }
+
 }
   
 function interact(x, y){
-    console.log(checklist);
-    if ("hello" in checklist){
-        log("CheckList: Hello")
+    console.log("Interaction")
+} 
+
+function roomFade(time, type){
+    //Lots of fade out statements
+    if (type == "Out"){
+        $("#Rooms").fadeOut(time);
+        $("#player").fadeOut(time);
+    } else if (type == "In"){
+        $("#Rooms").fadeIn(time);
+        $("#player").fadeIn(time);
     }
-}   
+}
+function roomChange(destination){
+    room = "hallway"
+    var delay = 1000;
+    roomFade(delay, "Out");
+    sleep(delay).then(() => { document.getElementById("Rooms").setAttribute("points", destination); });
+    roomFade(1500, "In");
+}
